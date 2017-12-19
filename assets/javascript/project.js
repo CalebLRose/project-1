34c1978df884880439ca13e26b2bc7d07ad0208b
Caleb Rose <clrose86@gmail.com>
2017-12-18 20:32:52 -0600

    added js

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	var venues = []
+
 function shows(){
 	var queryURL = "http://api.songkick.com/api/3.0/metro_areas/35129-us-dallas-fort-worth/calendar.json?apikey=ENjLM092JqaXsW2i";
 	
