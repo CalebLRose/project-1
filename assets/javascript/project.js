57fb9c72d7b74e208fb8071bf9f0ef79ce228eaf
Caleb Rose <clrose86@gmail.com>
2017-12-18 18:48:48 -0600

    first commit

---
+++ assets/javascript/project.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+function shows(){
+	var queryURL = "http://api.songkick.com/api/3.0/metro_areas/35129-us-dallas-fort-worth/calendar.json?apikey=ENjLM092JqaXsW2i";
+	
+	$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+	.done(function(response){
+		console.log(response);
+	})
+};
+
+shows();
+
+})
