0af530ecd6fb8331448099a1afb4644d7af77683
Caleb Rose <clrose86@gmail.com>
2017-12-20 18:58:58 -0600

    second push

---
+++ assets/javascript/data.js
@@ -0,0 +1,203 @@
+	var venues = [
+		{
+			name: "The Prophet Bar" ,
+			neighborhood: "Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"15191"
+		},
+		{
+			name: "The Bomb Factory",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"45544"
+		},
+		{
+			name:"Three Links",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2146544"
+		},
+		{
+			name:"Club Dada",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"11346"
+		},
+		{
+			name:"Canton Hall",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"3622809"
+		},
+		{
+			name:"Sons of Herman Hall",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"1988459"
+		},
+		{
+			name:"Double Wide",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"78786"
+		},
+		{
+			name:"Twilite Lounge",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2354464"
+		},
+		{
+			name:"Adair's Saloon",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2378869"
+		},
+		{
+			name:"R.B.C.",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2603828"
+		},
+		{
+			name:"Curtain Club",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"35191"
+		},
+		{
+			name:"It'll Do",
+			neighborhood:"Deep Ellum",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"1881283"
+		},
+		{
+			name:"Lizard Lounge",
+			neighborhood:"Deep Ellum",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"18"
+		},
+		{
+			name:"The Loft",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"10490"
+		},
+		{
+			name:"South Side Ballroom",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2262874"
+		},
+		{
+			name:"South Side Music Hall",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"1145321"
+		},
+		{
+			name:"Kessler Theater",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"1076266"
+		},
+		{
+			name:"The Wild Detectives",
+			neighborhood:"South Dallas",
+			parking:"Street",
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2547944"
+		},
+		{
+			name:"Small Brewpub",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"2907268"
+		},
+		{
+			name:"The Texas Theater",
+			neighborhood:"South Dallas",
+			parking:["Street","Lot"],
+			tixCost:,
+			drinkCost:,
+			cleanliness:,
+			extraInfo:,
+			id:"1356161"
+		},
+
+	]
--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
-	var venues = []
+  var config = {
+    apiKey: "AIzaSyBDm5nT9v8GJXej3TZwLHfkgt55HGXwOYA",
+    authDomain: "dfw-music.firebaseapp.com",
+    databaseURL: "https://dfw-music.firebaseio.com",
+    projectId: "dfw-music",
+    storageBucket: "",
+    messagingSenderId: "640782752126"
+  };
+  firebase.initializeApp(config);
 
 function shows(){
-	var queryURL = "http://api.songkick.com/api/3.0/metro_areas/35129-us-dallas-fort-worth/calendar.json?apikey=ENjLM092JqaXsW2i";
+	var id = "498771";
+	var queryURL = "http://api.songkick.com/api/3.0/venues/"+id+".json?apikey=ENjLM092JqaXsW2i";
 	
 	$.ajax({
 			url: queryURL,
