023c380400775980f4a1185e6fc37739bafc0e8c
Caleb Rose <clrose86@gmail.com>
2018-01-03 19:18:06 -0600

    user authorization and google sign in

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,387 +1,386 @@
-	var venues = [
-		{
-			name: "The Prophet Bar" ,
-			neighborhood: "Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"15191"
-		},
-		{
-			name: "The Bomb Factory",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"45544"
-		},
-		{
-			name:"Three Links",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2146544"
-		},
-		{
-			name:"Club Dada",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"11346"
-		},
-		{
-			name:"Canton Hall",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"3622809"
-		},
-		{
-			name:"Sons of Herman Hall",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"1988459"
-		},
-		{
-			name:"Double Wide",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"78786"
-		},
-		{
-			name:"Twilite Lounge",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2354464"
-		},
-		{
-			name:"Adair's Saloon",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2378869"
-		},
-		{
-			name:"R.B.C.",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2603828"
-		},
-		{
-			name:"Curtain Club",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"35191"
-		},
-		{
-			name:"It'll Do",
-			neighborhood:"Deep Ellum",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"1881283"
-		},
-		{
-			name:"Lizard Lounge",
-			neighborhood:"Deep Ellum",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"18"
-		},
-		{
-			name:"The Loft",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"10490"
-		},
-		{
-			name:"South Side Ballroom",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2262874"
-		},
-		{
-			name:"South Side Music Hall",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"1145321"
-		},
-		{
-			name:"Kessler Theater",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"1076266"
-		},
-		{
-			name:"The Wild Detectives",
-			neighborhood:"South Dallas",
-			parking:"Street",
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2547944"
-		},
-		{
-			name:"Small Brewpub",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"2907268"
-		},
-		{
-			name:"The Texas Theater",
-			neighborhood:"South Dallas",
-			parking:["Street","Lot"],
-			tixCost:,
-			drinkCost:,
-			cleanliness:,
-			extraInfo:,
-			id:"1356161"
-		},
-		{
-	name:"Granada",
-	neighborhood:"East Dallas",
-	parking:["street", "lot", "valet"],
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"11334",
+	// var venues = [
+	// 	{
+	// 		name: "The Prophet Bar" ,
+	// 		neighborhood: "Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"15191"
+	// 	},
+	// 	{
+	// 		name: "The Bomb Factory",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"45544"
+	// 	},
+	// 	{
+	// 		name:"Three Links",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2146544"
+	// 	},
+	// 	{
+	// 		name:"Club Dada",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"11346"
+	// 	},
+	// 	{
+	// 		name:"Canton Hall",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"3622809"
+	// 	},
+	// 	{
+	// 		name:"Sons of Herman Hall",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"1988459"
+	// 	},
+	// 	{
+	// 		name:"Double Wide",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"78786"
+	// 	},
+	// 	{
+	// 		name:"Twilite Lounge",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2354464"
+	// 	},
+	// 	{
+	// 		name:"Adair's Saloon",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2378869"
+	// 	},
+	// 	{
+	// 		name:"R.B.C.",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2603828"
+	// 	},
+	// 	{
+	// 		name:"Curtain Club",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"35191"
+	// 	},
+	// 	{
+	// 		name:"It'll Do",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"1881283"
+	// 	},
+	// 	{
+	// 		name:"Lizard Lounge",
+	// 		neighborhood:"Deep Ellum",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"18"
+	// 	},
+	// 	{
+	// 		name:"The Loft",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"10490"
+	// 	},
+	// 	{
+	// 		name:"South Side Ballroom",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2262874"
+	// 	},
+	// 	{
+	// 		name:"South Side Music Hall",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"1145321"
+	// 	},
+	// 	{
+	// 		name:"Kessler Theater",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"1076266"
+	// 	},
+	// 	{
+	// 		name:"The Wild Detectives",
+	// 		neighborhood:"South Dallas",
+	// 		parking:"Street",
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2547944"
+	// 	},
+	// 	{
+	// 		name:"Small Brewpub",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"2907268"
+	// 	},
+	// 	{
+	// 		name:"The Texas Theater",
+	// 		neighborhood:"South Dallas",
+	// 		parking:["Street","Lot"],
+	// 		tixCost:,
+	// 		drinkCost:,
+	// 		cleanliness:,
+	// 		extraInfo:,
+	// 		id:"1356161"
+	// 	},
+	// 	{
+	// 		name:"Granada",
+	// 		neighborhood:"East Dallas",
+	// 		parking:["street", "lot", "valet"],
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"11334",
 
-		},
-	name:"Sundown at Granada",
-	neighborhood:"East Dallas",
-	parking:["street", "lot", "valet"],
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"1773188",
-	},
-	{
-	name:"The Green Elephant",
-	neighborhood:"East Dallas",
-	parking:"street",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"103402",
-	},
-	{
-	name:"Balcony Club",
-	neighborhood:"East Dallas";
-	parking:"lot",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"314721",
-	},
-	{
-	name:"Single Wide",
-	neighborhood:"East Dallas",
-	parking:["street", "lot", "valet"]
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"1552943"
-	},
-	{
-	name:"The Goat",
-	neighborhood:"East Dallas",
-	parking:"lot",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"1203686",
-	},
-	{
-	name:"Post Time",
-	neighborhood:"East Dallas",
-	parking:
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"2008879",
-	},
+	// 	},
+	// 	{
+	// 		name:"Sundown at Granada",
+	// 		neighborhood:"East Dallas",
+	// 		parking:["street", "lot", "valet"],
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"1773188",
+	// 	},
+	// 	{
+	// 		name:"The Green Elephant",
+	// 		neighborhood:"East Dallas",
+	// 		parking:"street",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"103402",
+	// 	},
+	// 	{
+	// 		name:"Balcony Club",
+	// 		neighborhood:"East Dallas";
+	// 		parking:"lot",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"314721",
+	// 	},
+	// 	{
+	// 		name:"Single Wide",
+	// 		neighborhood:"East Dallas",
+	// 		parking:["street", "lot", "valet"]
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"1552943"
+	// 	},
+	// 	{
+	// 		name:"The Goat",
+	// 		neighborhood:"East Dallas",
+	// 		parking:"lot",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"1203686",
+	// 	},
+	// 	{
+	// 		name:"Post Time",
+	// 		neighborhood:"East Dallas",
+	// 		parking:
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"2008879",
+	// 	},
+	// 	{
+	// 		name:"White Rock Coffee",
+	// 		neighborhood:"East Dallas",
+	// 		parking:"lot",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"776731",
+	// 	},
+	// 	{
+	// 		name:"Majestic Theatre",
+	// 		id:""
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:["street", "lot", "valet"],
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"498771",
+	// 	},
+	// 	{
+	// 		name:"Panopticon",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"3570824",
+	// 	},
+	// 	{
+	// 		name:"City Tavern",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:"street",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"618756",
+	// 	},
+	// 	{
+	// 		name:"One Eyed Penguin",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:"street",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"",
+	// 	},
+	// 	{
+	// 		name:"House of Blues",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:["street", "lot", "valet"],
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"36239",
+	// 	},
+	// 	{
+	// 		name:"Nickle and Rye",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:""
+	// 	},
+	// 	{
+	// 		name:"Rustic",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"2453599",
+	// 	},
+	// 	{
+	// 		name:"Foundry",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:"lot",
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"1773198",
+	// 	},
+	// 	{
+	// 		name:"Stereo Live",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"",
+	// 	},
+	// 	{
+	// 		name:"Gas Monkey",
+	// 		neighborhood:"Downtown & Uptown",
+	// 		parking:["valet","lot"]
+	// 		tixCost:
+	// 		drinkCost:
+	// 		cleanliness:
+	// 		extraInfo:
+	// 		id:"2424769",
+	// 	},
 
-	{
-	name:"White Rock Coffee",
-	neighborhood:"East Dallas",
-	parking:"lot",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"776731",
-	},
 
-	{
-	name:"Majestic Theatre",
-	id:""
-	neighborhood:"Downtown & Uptown",
-	parking:["street", "lot", "valet"],
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"498771",
-	}
-	{
-	name:"Panopticon",
-	neighborhood:"Downtown & Uptown",
-	parking:
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"3570824",
-	},
-	{
-	name:"City Tavern",
-	neighborhood:"Downtown & Uptown",
-	parking:"street",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"618756",
-	},
-	{
-	name:"One Eyed Penguin",
-	neighborhood:"Downtown & Uptown",
-	parking:"street",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"",
-	},
-	{
-	name:"House of Blues",
-	neighborhood:"Downtown & Uptown",
-	parking:["street", "lot", "valet"],
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"36239",
-	},
-	{
-	name:"Nickle and Rye",
-	neighborhood:"Downtown & Uptown",
-	parking:
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:""
-	},
-	{
-	name:"Rustic",
-	neighborhood:"Downtown & Uptown",
-	parking:
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"2453599",
-	},
-	{
-	name:"Foundry",
-	neighborhood:"Downtown & Uptown",
-	parking:"lot",
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"1773198",
-	},
-	{
-	name:"Stereo Live",
-	neighborhood:"Downtown & Uptown",
-	parking:
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"",
-	},
-	{
-	name:"Gas Monkey",
-	neighborhood:"Downtown & Uptown",
-	parking:["valet","lot"]
-	tixCost:
-	drinkCost:
-	cleanliness:
-	extraInfo:
-	id:"2424769",
-	},
-
-
-	]
+	// ]
--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,28 +1,69 @@
 $(document).ready(function(){
 
-  var config = {
-    apiKey: "AIzaSyBDm5nT9v8GJXej3TZwLHfkgt55HGXwOYA",
-    authDomain: "dfw-music.firebaseapp.com",
-    databaseURL: "https://dfw-music.firebaseio.com",
-    projectId: "dfw-music",
-    storageBucket: "",
-    messagingSenderId: "640782752126"
-  };
-  firebase.initializeApp(config);
+	var config = {
+	    apiKey: "AIzaSyBDm5nT9v8GJXej3TZwLHfkgt55HGXwOYA",
+	    authDomain: "dfw-music.firebaseapp.com",
+	    databaseURL: "https://dfw-music.firebaseio.com",
+	    projectId: "dfw-music",
+	    storageBucket: "dfw-music.appspot.com",
+	    messagingSenderId: "640782752126"
+	  };
+	firebase.initializeApp(config);
 
-function shows(){
-	var id = "498771";
-	var queryURL = "http://api.songkick.com/api/3.0/venues/"+id+".json?apikey=ENjLM092JqaXsW2i";
+	var userBase = firebase.auth();
+	var userName;
+	var userPassWrd;
+	var userEmail;
+
+	$("#googleAuth").on("click",function(){
+		// event.preventDefault();
+		var provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider).then(function(result) {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			// ...
+		}).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+			// ...
+		});
+	});
+		
+
+	$("#submitAcct").on("click",function(){
+  		event.preventDefault();
+  		userEmail = $("#userEmail").val().trim();
+  		console.log(userEmail);
+  		userPassWrd = $("#userPassword").val().trim();
+  		console.log(userPassWrd);
+  		userBase.createUserWithEmailAndPassword(userEmail,userPassWrd).catch(function(error) {
+		  // Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		});
+  });
+
+	function shows(){
+		var id = "498771";
+		var queryURL = "http://api.songkick.com/api/3.0/venues/"+id+".json?apikey=ENjLM092JqaXsW2i";
 	
-	$.ajax({
+		$.ajax({
 			url: queryURL,
 			method: "GET"
 		})
 	.done(function(response){
 		console.log(response);
 	})
-};
+	};
 
-shows();
+	shows();
+
 
 })
