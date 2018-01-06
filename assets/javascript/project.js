e008d7df361356cb867113c99d0488ebc12b1d39
Caleb Rose <clrose86@gmail.com>
2018-01-06 10:14:42 -0600

    styling, auth, fonts added

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -11,44 +11,104 @@ $(document).ready(function(){
 	firebase.initializeApp(config);
 
 	var userBase = firebase.auth();
-	var userName;
-	var userPassWrd;
+	var user = firebase.auth().currentUser;
 	var userEmail;
+	var userPswrd;
 
-	$("#googleAuth").on("click",function(){
-		// event.preventDefault();
-		var provider = new firebase.auth.GoogleAuthProvider();
-		firebase.auth().signInWithRedirect(provider).then(function(result) {
-			// This gives you a Google Access Token. You can use it to access the Google API.
-			var token = result.credential.accessToken;
-			// The signed-in user info.
-			var user = result.user;
-			// ...
-		}).catch(function(error) {
-			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			// The email of the user's account used.
-			var email = error.email;
-			// The firebase.auth.AuthCredential type that was used.
-			var credential = error.credential;
-			// ...
-		});
+	// CHECKS TO SEE IF A USER is signed in
+	firebase.auth().onAuthStateChanged(function(user){
+		if(user){
+			console.log(user);
+			$("#userNav").css("display","inline-block");
+			$("#outBtn").css("display","inline-block");
+			$("#signBtn").css("display","none");
+			console.log(userBase.currentUser.email);
+			$("#userNav").text(userBase.currentUser.displayName);
+			console.log("displayName: "+userBase.currentUser.displayName);
+		}
 	});
-		
 
+// SIGN IN FUNCTION
+	function signIn(){
+		firebase.auth().signInWithEmailAndPassword(userEmail, userPswrd).catch(function(error) {
+		  // Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		  // ...
+		});
+	};
+
+	$("#signInBtn").on("click", function(){
+		userEmail = $("#userEmail").val().trim();
+		userPswrd = $("#userPassword").val().trim();
+		signIn();
+	});
+
+	// $("#googleAuth").on("click",function(){
+	// 	// event.preventDefault();
+	// 	var provider = new firebase.auth.GoogleAuthProvider();
+	// 	firebase.auth().signInWithRedirect(provider).then(function(result) {
+	// 		// This gives you a Google Access Token. You can use it to access the Google API.
+	// 		var token = result.credential.accessToken;
+	// 		// The signed-in user info.
+	// 		var user = result.user;
+	// 		// ...
+	// 	}).catch(function(error) {
+	// 		// Handle Errors here.
+	// 		var errorCode = error.code;
+	// 		var errorMessage = error.message;
+	// 		// The email of the user's account used.
+	// 		var email = error.email;
+	// 		// The firebase.auth.AuthCredential type that was used.
+	// 		var credential = error.credential;
+	// 		// ...
+	// 	});
+	// });
+
+	// SIGN OUT
+	$("#outBtn").on("click",function(){
+		$("#userNav").css("display","none");
+		$("#outBtn").css("display","none");
+		$("#signBtn").css("display","inline-block");
+		firebase.auth().signOut().then(function() {
+		  console.log('Signed Out');
+		}, function(error) {
+		  console.error('Sign Out Error', error);
+		});
+	});	
+
+
+		
 	$("#submitAcct").on("click",function(){
   		event.preventDefault();
-  		userEmail = $("#userEmail").val().trim();
-  		console.log(userEmail);
-  		userPassWrd = $("#userPassword").val().trim();
-  		console.log(userPassWrd);
-  		userBase.createUserWithEmailAndPassword(userEmail,userPassWrd).catch(function(error) {
+  		var createEmail = $("#createEmail").val().trim();
+  		console.log(createEmail);
+  		var createPswrd = $("#createPassword").val().trim();
+  		console.log(createPswrd);
+  		userBase.createUserWithEmailAndPassword(createEmail,createPswrd).catch(function(error) {
 		  // Handle Errors here.
 		var errorCode = error.code;
 		var errorMessage = error.message;
 		});
-  });
+		userEmail = createEmail;
+		userPswrd = createEmail;
+		signIn();
+  		var createName = $("#createUsername").val().trim();
+  		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				user.updateProfile({
+					displayName: createName,
+					}).then(function() {
+					  // Update successful.
+					  console.log("display name "+displayName);
+					}).catch(function(error) {
+					  // An error happened.
+				});
+			}
+		});
+  	});
+
+
 
 	function shows(){
 		var id = "498771";
